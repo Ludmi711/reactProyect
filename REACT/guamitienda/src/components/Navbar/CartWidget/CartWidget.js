ba367a8ca2b9e978de8f6668cb8ea8475884fe30
Ludmila Alegre <ludmilaaalegre@gmail.com>
2022-11-23 23:57:54 -0300

    primera entrega

---
+++ REACT/guamitienda/src/components/Navbar/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import '../navbar.css'
+const CartWidget = () => {
+    return (
+        <div className="carrito">
+            <button>
+                <p>🛒</p>
+            </button>
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ REACT/guamitienda/src/components/Navbar/navbar.js
@@ -0,0 +1,18 @@
+import './navbar.css'
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Guami Tienda Online</h1>
+            <div id="opciones">
+                <ul>
+                    <li><button>Inicio</button> </li>
+                    <li><button>Nosotros</button> </li>
+                    <li><button>Tienda</button> </li>
+                    <li><button>Contacto</button> </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
